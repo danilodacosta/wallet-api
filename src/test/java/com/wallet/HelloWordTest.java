5b5bc0da6b48aad17f85ae88a3f84a97400c4a4d
danilodacosta <danilo-gc@hotmail.com>
2023-06-22 01:36:17 -0300

    adding initial config for testing

--- src/test/java/com/wallet/HelloWordTest.java
+++ src/test/java/com/wallet/HelloWordTest.java
@@ -1,18 +1,17 @@
 package com.wallet;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-public class WalletApplicationTests {
+public class HelloWordTest {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void testHelloWord() {
+        assertEquals(1,1);
+    }
 }
